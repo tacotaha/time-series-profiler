ed8d9697efb9601363274389dcf9e31c97348fb1
Taha Azzaoui <tazzaoui@cs.uml.edu>
2018-09-08 19:53:33 -0400

    Moved to switch

--- Main.c
+++ Main.c
@@ -11,21 +11,29 @@
 
 int main(int argc, char* argv[]){
     pid_t pid = fork();
-    if(pid < 0){
-        printf("Error launching child!\n");
+    int status = 0;
+    const char* program = "/bin/sleep";
+    const char* argument = "5";
+    
+    switch(pid){
+    case -1:
+        /* Error Forking */
+        printf("Error Launching Child!\n");
         exit(1);
-    }else if(pid == 0){
-        const char* program = "/bin/sleep";
-        const char* argument = "5";
-        int status = execl(program, program, argument, (char*) NULL);
+    case 0:
+        status = execl(program, program, argument, (char*) NULL);
         if(status < 0){
             printf("Failed to start program!\n");
             exit(1);
         }else printf("Child process started: status = %d\n", status);
-    }else{ 
+        break;
+    default:
         printf("master, child pid: %d\n", pid);
         printf("Memory Usage of %d is %dKB\n", pid, get_mem_usage(pid));
         waitpid(pid, NULL, 0);
+        break;
     }
+
+
     return 0;
 }
