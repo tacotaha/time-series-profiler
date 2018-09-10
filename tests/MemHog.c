a9b343c5cfc643facfbe1a3e475d1ce732556f66
Taha Azzaoui <tazzaoui@cs.uml.edu>
2018-09-10 00:27:49 -0400

    Updated test

--- tests/MemHog.c
+++ tests/MemHog.c
@@ -2,12 +2,14 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include "../src/Meminfo.h"
+#include "../src/CPUinfo.h"
 
 int main() {
   int n = 100;
   int entrySize = 10000000;
   int* buffer[n];
   mem_usage_t mem;
+  cpu_usage_t cpu;
   pid_t pid = getpid();
 
   for (int i = 0; i < n; i++) {
@@ -20,9 +22,13 @@ int main() {
 
     for (int j = 0; j < entrySize; j++) buffer[i][j] = 0;
 
+    cpu.pid = pid;
+    get_cpu_usage(&cpu);
     get_mem_usage(&mem, pid);
     printf("%2d: Current memory usage: data = %6ld KB, stack = %6ld KB\n", 
             i, mem.vm_data_kb, mem.vm_stack_kb);
+    printf("%2d: Current CPU usage: user = %.2lf\%, system = %.2lf\%\n", 
+            i, cpu.user, cpu.system);
   }
 
   return 0;
