2f58d8dfd27cf5b61cd12a92c8d085315358ec2e
Taha Azzaoui <tazzaoui@cs.uml.edu>
2018-09-17 10:49:06 -0400

    Formatting

--- src/Meminfo.h
+++ src/Meminfo.h
@@ -1,22 +1,22 @@
 #ifndef MEMINFO_H
-#define MEMINFO_H 
+#define MEMINFO_H
 
 #include <sys/types.h>
 #define BUF 4096
 
-typedef struct mem_usage_t{
-    pid_t pid;
-    unsigned long vm_stack_kb;
-    unsigned long vm_data_kb; 
-}mem_usage_t;
+typedef struct mem_usage_t {
+  pid_t pid;
+  unsigned long vm_stack_kb;
+  unsigned long vm_data_kb;
+} mem_usage_t;
 
 /*
-* Extract the memory usage from lines in /proc/pid/status that
-* look like
-*
-* VmRSS:          5560 kB
-* VmSize:         5560 kB
-*/
+ * Extract the memory usage from lines in /proc/pid/status that
+ * look like
+ *
+ * VmRSS:          5560 kB
+ * VmSize:         5560 kB
+ */
 int get_mem_usage(mem_usage_t*);
 
 #endif /* MEMINFO_H */
